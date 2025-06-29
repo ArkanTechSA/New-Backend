e7fa561c174d4888550143335c23024e3703b2cd
ibrahim ahmed <84781424+ibraimfarag@users.noreply.github.com>
2025-06-29 14:01:05 +0300

    Remove package annotation from the PHP file header

---
+++ .php-cs-fixer.php
@@ -0,0 +1,26 @@
+<?php
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+$finder = Finder::create()
+    ->in([
+        __DIR__.'/app',
+        __DIR__.'/config',
+        __DIR__.'/database',
+        __DIR__.'/resources',
+        __DIR__.'/routes',
+   
+    ])
+    ->name('*.php')
+    ->notName('*.blade.php')
+    ->ignoreDotFiles(true)
+    ->ignoreVCS(true);
+
+return (new Config())
+    ->setRules([
+        '@PSR2' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'no_unused_imports' => true,
+    ])
+    ->setFinder($finder);
